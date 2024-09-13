ad67286964250198e5cd77920a066a5529b94aba
Sv443 <contact@sv443.net>
2024-09-14 01:11:56 +0200

    feat: custom string replacement vite plugin & constants.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import UserscriptPlugin from "vite-userscript-plugin";
 import TSConfigPathsPlugin from "vite-tsconfig-paths";
 import packageJson from "./package.json" with { type: "json" };
+import { execSync } from "child_process";
 
 const { author, homepage, namespace, repository, userscriptName, version } = packageJson;
 
@@ -33,6 +34,8 @@ const devServerPort = !isNaN(cliPortRaw)
 
 
 export default defineConfig(({ mode }) => {
+  const buildNbr = getCommitSha();
+
   return {
     build: {
       minify: false,
@@ -76,30 +79,64 @@ export default defineConfig(({ mode }) => {
             "https://youtube.com/*",
             "https://music.youtube.com/*",
           ],
-          icon: getResourceUrl(mode, "assets/plugin_icon_128x128.png"),
-          // since the .meta.js is not committed, updating via that URL is not possible
-          updateURL: undefined,
+          icon: getResourceUrl(mode, "assets/plugin_icon_128x128.png", buildNbr),
           resource: [
-            ["icon_1000", getResourceUrl(mode, "assets/plugin_icon_1000x1000.png")],
-            ["icon_128", getResourceUrl(mode, "assets/plugin_icon_128x128.png")],
+            ["icon_1000", getResourceUrl(mode, "assets/plugin_icon_1000x1000.png", buildNbr)],
+            ["icon_128", getResourceUrl(mode, "assets/plugin_icon_128x128.png", buildNbr)],
           ]
         },
+      }),
+      replaceStringsPlugin({
+        "#{{BUILD_MODE}}": mode,
+        "#{{BUILD_NUMBER}}": buildNbr,
       }),
     ],
   };
 });
 
+/** Replaces strings in the bundle with other strings. */
+function replaceStringsPlugin(options: Record<string, string>): Plugin {
+  return {
+    name: "vite-plugin-custom-replace-strings",
+    transform(code, _id) {
+      for(const [searchValue, replaceValue] of Object.entries(options)) {
+        const regex = new RegExp(searchValue, "gm");
+        code = code.replace(regex, replaceValue);
+      }
+      return { code };
+    }
+  };
+}
+
+/**
+ * Returns the commit sha of the latest commit for use as a build number.  
+ *   
+ * ⚠️ Important: This will always trail behind the current commit by one, as the act of committing this number will also change it.  
+ * If your script depends on this number (for example for versioned GitHub asset URLs), you should always commit your build separately and last.
+ */
+function getCommitSha() {
+  try {
+    return execSync("git rev-parse --short HEAD").toString().trim();
+  }
+  catch {
+    console.error("\x1b[31mFailed to get the commit SHA. Is Git installed?\x1b[0m\nFalling back to 'BUILD_ERROR'.");
+    return "BUILD_ERROR";
+  }
+}
+
 /**
  * Returns the URL to a resource.  
  * In `development` mode, the resource is served by the dev server.  
- * In `production` mode, the resource is fetched from the repository's `main` branch.
+ * In `production` mode, the resource is fetched using the given commit SHA or branch name (`main` by default).
  * @param mode `development` or `production`, defaults to `development`
  * @param path The path to the resource relative to the repository root
+ * @param buildNbrOrBranch The build number or branch name to use in the URL, defaults to `main` - this is very useful for versioned asset URLs, which will never break by changes made to the `main` branch.
  */
-function getResourceUrl(mode: string, path: string) {
+function getResourceUrl(mode: string, path: string, buildNbrOrBranch: string = "main") {
   if(path.startsWith("/"))
     path = path.slice(1);
+
   return mode === "development"
     ? `http://localhost:${devServerPort}/${path}`
-    : `https://raw.githubusercontent.com/${repo}/main/${path}`;
+    : `https://raw.githubusercontent.com/${repo}/${buildNbrOrBranch}/${path}`;
 }
