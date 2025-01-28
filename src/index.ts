b5588ca8f14a1ebb934586f1ff0f785e84df16d8
Sv443 <contact@sv443.net>
2025-01-28 13:17:17 +0100

    ref: clarified the example code a bit

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,30 @@ import { log } from "@utils/logging.js";
 import { buildNumber, buildMode } from "@utils/constants.js";
 import { examplePreInit } from "@/example/preInit.js";
 import { exampleMainEntrypoint } from "@/example/main.js";
-import { someJsdocFunction, somePlainFunction } from "@/example/plainJs.mjs";
+import { jsdocPow, plainJsSetFactor } from "@/example/plainJs.mjs";
 import "@/types.js";
 
 // #region register plugin
-// This is the entry point of your plugin:
+
+// this is the entry point of your plugin:
 unsafeWindow.addEventListener("bytm:registerPlugin", async (registerPlugin) => {
   try {
-    // Call a few functions that need to be run as soon as possible:
+    // call a few functions that need to be run as soon as possible:
     preInit();
 
     try {
-      // Register the plugin with BetterYTM to be able to call authenticated API functions:
+      // register the plugin with BetterYTM to be able to call authenticated API functions:
       await tryRegisterPlugin(registerPlugin);
       log(`Registered plugin successfully!\nUsing the BetterYTM API v${unsafeWindow.BYTM.version}\nPlugin build number: ${buildNumber} (${buildMode} mode)`);
+
+      // run the main code of your plugin once BYTM and the DOM are ready:
+      events.once("bytm:ready", run);
     }
     catch(err) {
-      console.error("Couldn't register plugin:", err);
+      alert("Couldn't register the plugin. Refer to the console for more information.");
+      console.error("Couldn't register plugin due to error:", err);
+      return;
     }
-
-    // Run the main code of your plugin once BYTM and the DOM are ready:
-    events.once("bytm:ready", run);
   }
   catch(err) {
     console.error("A generic error occurred:", err);
@@ -33,21 +36,32 @@ unsafeWindow.addEventListener("bytm:registerPlugin", async (registerPlugin) => {
 // #region preInit
 /** You can do anything in here that needs to be done as soon as the page loads, except modifying the DOM. */
 function preInit() {
-  // Check out this example code in src/example/preInit.ts:
+  // check out this example code in src/example/preInit.ts:
   examplePreInit();
 
-  // example of calling functions from the plain JS module src/example/plainJs.mjs:
-  const foo = someJsdocFunction(9);                     // this one has a typed argument and typed return value
-  const bar = somePlainFunction("any type is allowed"); // this one accepts any type but still has a typed return value
+  // example calling functions from the plain JS module src/example/plainJs.mjs
 
-  log("foo:", foo, "bar:", bar);
+  // this one accepts any type, making it prone to bugs:
+  const foo = plainJsSetFactor("hello");
+  //    ^?: string
+
+  const factor = plainJsSetFactor(2);
+  //    ^?: number
+
+  // this one has its argument and return value typed, so the IDE gives hints about the expected types:
+  const value = jsdocPow(9);
+  //    ^?: number
+
+  log("factor:", factor, "value:", value); // 2, 81
+
+  void foo;
 }
 
 // #region main
 /** This function gets called whenever the plugin is fully registered and the DOM is available. */
 async function run() {
-  // This is where you can modify the DOM, add event listeners, etc.
+  // this is where you can modify the DOM, add event listeners, etc.
 
-  // Check out this example code in src/example/main.ts:
+  // check out this example code in src/example/main.ts:
   await exampleMainEntrypoint();
 }
